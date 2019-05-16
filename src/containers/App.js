c795d2baf90e46c7425c65981147717e1fa430a7
lbarros00 <laisa.barros26@gmail.com>
2019-05-16 01:11:59 -0400

    fix cart button message to number of items

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
+import { connect } from 'react-redux'
+import { checkout } from '../actions'
+import { getTotal, getCartProducts } from '../reducers'
 import '../styles/cart.css'
 
 class App extends React.Component {
@@ -17,12 +21,12 @@ class App extends React.Component {
       show: !this.state.show
     });
   }
-  
+
   render() {
     return (
 	  <div>
 	    <h2>Acme Store 
-		<button id="cart-btn" onClick={()=>this.togglePopup()}> <span id="cart-msg">Your cart is empty</span></button>
+		<button id="cart-btn" onClick={()=>this.togglePopup()}> <span id="cart-msg"> {this.props.products.length == 0 ? "Your cart is empty" : this.props.products.length}</span></button>
 	    </h2>
 	    <hr/>
 	    <ProductsContainer />
@@ -30,4 +34,17 @@ class App extends React.Component {
 	  </div>)}
 }
 
-export default App
+App.propTypes = {
+  total: PropTypes.string,
+  checkout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  products: getCartProducts(state),
+  total: getTotal(state)
+})
+
+export default connect(
+  mapStateToProps,
+  { checkout }
+)(App)
